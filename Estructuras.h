dfdb1cc4342203fa4d45dba0adc337529ba38a2c
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-31 03:38:52 -0500

    Estructuras.h

---
+++ Estructuras.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Estructuras{
+public:
+
+
+  Estructuras(string* nota,int posi,int tama);
+
+  void tono (int&);
+  void semitono(int&);
+
+  void EscalaMayor();
+
+
+  void print();
+
+  void EscalaMenor();
+  void intervalos();
+  void Conversor();
+  void dorica();
+  void frigia();
+  void Cromatica();
+  void lidia();
+  void lidiaAumentada();
+  void PentatonicaMenor();
+
+protected:
+  string*notas;
+  //[]=      {"Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si","Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si"};
+  int pos;
+  int tam=24;
+
+
+};
