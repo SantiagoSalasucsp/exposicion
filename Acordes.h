3222e46d986d3ed4839364f9912308a005e5e603
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-31 08:57:11 -0500

    Update Acordes.h

--- Acordes.h
+++ Acordes.h
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include "Estructuras.h"
 using namespace std;
-class Acordes:public Estructuras{
+class Acordes:public Estructuras {
 
 public:
 Acordes(string* no,int posi,int tama);
