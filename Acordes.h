603affc410aec2018197c191ab51d41704657314
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-31 03:37:11 -0500

    Acordes.h

---
+++ Acordes.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Acordes:public Estructuras{
+
+public:
+Acordes(string* no,int posi,int tama);
+void acordeMayor();
+void acorrdeMenor();
+
+
+
+
+};
