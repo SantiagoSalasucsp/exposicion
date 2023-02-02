1398ad1f34fd836109ab678ef80a0f087a568326
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-02-02 00:00:22 -0500

    Create Trasformador.h

---
+++ Trasformador.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Trasformador{
+
+private:
+    int NotaInicial;
+    int trasformador;
+
+
+public:
+    Trasformador(int, int);
+
+    void Trasformar();
+    
+};
