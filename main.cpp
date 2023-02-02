f9a6e33b55de3982c233fff751ff1592b1af11a4
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-02-01 23:59:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
-#include <iostream>
+include <iostream>
 #include <string>
 #include "Estructuras.h"
 using namespace std;
 
 int main() {
-  int i;
- string notas[]={"Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si","Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si"};
 
+  int Do=0;
+  int Reb=1;
+  int Re=2;
+  int Mib=3;
+  int Mi=4;
+  int Fa=5;
+  int Solb=6;
+  int Sol=7;
+  int Lab=8;
+  int La=9;
+  int Sib=10;
+  int Si=11;
 
-
-  
-  
-cout<<endl;
-cout<<endl;
-cout<<endl;
-cout<<endl;
-
+ //string notas[]={"Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si","Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si"};
+ string notas[]={"Do","Do#/Reb","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol","Sol#/Lab","La","La#/Sib","Si"};
 //EscalaMayor(notas,7);
 //cout<<endl;
 //EscalaMenor(notas,4);
 string* nota;
 nota=notas;
-Estructuras e1(nota,0,24);
-
+Estructuras e1(nota,La,11);
 e1.EscalaMayor();
+
+
+
 }
