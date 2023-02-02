d3cef414fd5ff348e4564ab8af57f269e35aefc8
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-02-02 00:00:58 -0500

    Create Trasformador.cpp

---
+++ Trasformador.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Trasformador.h"
+using namespace std;
+
+Trasformador::Trasformador(int x,int y){
+    NotaInicial=x;
+    trasformador=y;}
+
+void Trasformador::Trasformar(){
+    int i;
+    i=NotaInicial-trasformador;
+
+}
